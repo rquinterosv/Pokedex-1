f45a6733c1de10f1d5ba997cef15b37047d43bf4
rquinterosv <rafael.quinterosv@gmail.com>
2022-03-21 22:23:27 -0300

    entregando el proyecto final

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -3,3 +3,7 @@
 
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
+
+$('#button-container').click(function(evento){
+    var pokemon = evento.target.dataset.pokemon;
+});
